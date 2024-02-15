93d0eec54030749aec9d827a9bbdca31db84ab02
Shahriyor Mahsudaliyev <151597441+shahriyordevoloper@users.noreply.github.com>
2024-02-15 10:52:05 +0500

    update

--- chat/urls.py
+++ chat/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path("tables_all/",tables ,name='tables_all' ), 
     path("comingtype/",comingtype ,name='comingtype' ), 
     path("delete/<int:id>/",delete ,name='delete' ), 
+    path("delete1/<int:id>/",delete1 ,name='delete1' ), 
+    path("delete2/<int:id>/",delete2 ,name='delete2' ), 
     path("update_sick/<int:id>/",update_sick ,name='update_sick' ), 
+    path("update_sick1/<int:id>/",update_sick1 ,name='update_sick1' ), 
+    path("update_sick2/<int:id>/",update_sick2 ,name='update_sick2' ), 
 
 ]
--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse,JsonResponse
 import datetime
 
 def home(request):
-    print('\n' , datetime.datetime.now() , 'dddddddddddddddddddddddddddd', '\n')
+    # print('\n' , datetime.datetime.now() , 'dddddddddddddddddddddddddddd', '\n')
 
 
     if request.user.is_active:
@@ -53,14 +53,46 @@ def user_create(request):
 
 
 def comingtype(request):
-    
-    return render(request , 'mein/comingtype.html',{'data':    Sick.objects.all().filter(is_xuruj=True,user=request.user).order_by('-id')
-})
+    all_bemor = Sick.objects.all().filter(user=request.user).order_by('-date_times')
+    yoq = Sick.objects.filter(is_xuruj=True,user=request.user)
+
+
+
+
+    data_from =request.GET.get('date_from')
+    data_to = request.GET.get('date_to')
+    hurujs = Sick.objects.filter(date_times__gte='2000-02-06 00:00'  ,date_times__lt='4024-02-06 23:59',user=request.user ).order_by('-id')
+    if data_from and data_to :
+        hurujs = Sick.objects.filter(date_times__gte=f'{data_from} 00:00'  ,date_times__lt=f'{data_to} 23:59',user=request.user ).order_by('-id')
+    else:
+        hurujs = Sick.objects.filter(date_times__gte='2000-02-06 00:00'  ,date_times__lt='4024-02-06 23:59',user=request.user ).order_by('-id')
+
+
+
+    return render(request , 'mein/comingtype.html',{'data': hurujs.filter(is_xuruj=True)})
 
 def tables(request):
+    all_bemor = Sick.objects.all().filter(user=request.user).order_by('-date_times')
+    yoq = Sick.objects.filter(is_xuruj=True,user=request.user)
+
+
+
+
+    data_from =request.GET.get('date_from')
+    data_to = request.GET.get('date_to')
+    hurujs = Sick.objects.filter(date_times__gte='2000-02-06 00:00'  ,date_times__lt='4024-02-06 23:59',user=request.user ).order_by('-id')
+    if data_from and data_to :
+        hurujs = Sick.objects.filter(date_times__gte=f'{data_from} 00:00'  ,date_times__lt=f'{data_to} 23:59',user=request.user ).order_by('-id')
+    else:
+        hurujs = Sick.objects.filter(date_times__gte='2000-02-06 00:00'  ,date_times__lt='4024-02-06 23:59',user=request.user ).order_by('-id')
+
+
+
+
+
+        # return render(request , 'mein/index.html', {'all_bemor':all_bemor,'yoq':yoq , 'huruj':hurujs   })
     
-    return render(request , 'mein/tables-general.html',{'data':    Sick.objects.all().filter(user=request.user).order_by('-id')
-})
+    return render(request , 'mein/tables-general.html',{'huruj':    hurujs })
 
 
 
@@ -68,6 +100,15 @@ def delete(request,id):
     d = Sick.objects.get(id=id)
     d.delete()
     return redirect('/')
+def delete1(request,id):
+    d = Sick.objects.get(id=id)
+    d.delete()
+    return redirect('tables_all')
+
+def delete2(request,id):
+    d = Sick.objects.get(id=id)
+    d.delete()
+    return redirect('comingtype')
 
 def update_sick(request,id):
     sick = Sick.objects.get(id=id)
@@ -76,12 +117,34 @@ def update_sick(request,id):
     sick.dori_miqdor = request.GET.get('dori_miqdori')
     sick.is_xuruj = request.GET.get('talvasa')
     sick.date_times = datetime.datetime.now()
+
     
     sick.save()
-
     return redirect('/')
 
+def update_sick1(request,id):
+    sick = Sick.objects.get(id=id)
+    sick.username = request.GET.get('username')
+    sick.dori_name = request.GET.get('dori')
+    sick.dori_miqdor = request.GET.get('dori_miqdori')
+    sick.is_xuruj = request.GET.get('talvasa')
+    sick.date_times = datetime.datetime.now()
 
+    
+    sick.save()
+    return redirect('/tables_all/')
+
+def update_sick2(request,id):
+    sick = Sick.objects.get(id=id)
+    sick.username = request.GET.get('username')
+    sick.dori_name = request.GET.get('dori')
+    sick.dori_miqdor = request.GET.get('dori_miqdori')
+    sick.is_xuruj = request.GET.get('talvasa')
+    sick.date_times = datetime.datetime.now()
+
+    
+    sick.save()
+    return redirect('comingtype')
 
 
 
